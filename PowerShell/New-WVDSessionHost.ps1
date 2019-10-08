b8f95a76d1b3ee2737ba09295da3404b97751ddf
Dean Cefola <deacef@microsoft.com>
2019-10-08 17:37:00 -0400

    initial push (WIP)
    WIP push

--- PowerShell/New-WVDSessionHost.ps1
+++ PowerShell/New-WVDSessionHost.ps1
@@ -14,6 +14,8 @@
 # 09/17/2019                     3.1        Add code to disable IESEC for admins
 # 09/20/2019                     3.2        Add code to discover OS (Server / Client)
 # 09/20/2019                     4.0        Add code for servers to add RDS Host role
+# 10/01/2019                     4.1        Add Windows 7 Support
+# 10/07/2019                     4.2        Add all FSLogix Reg entries for easier management
 #
 #*********************************************************************************
 #
@@ -23,7 +25,7 @@
 ##############################
 #    WVD Script Parameters   #
 ##############################
-Param (    
+Param (        
     [Parameter(Mandatory=$true)]
         [string]$ProfilePath,
     [Parameter(Mandatory=$true)]
@@ -34,13 +36,21 @@ Param (
 ######################
 #    WVD Variables   #
 ######################
-$WVDBootURI = 'https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrxrH'
-$WVDAgentURI = 'https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrmXv'
-$FSLogixURI = 'https://go.microsoft.com/fwlink/?linkid=2084562'
-$WVDAgentInstaller = 'WVD-Agent.msi'
-$WVDBootInstaller = 'WVD-Bootloader.msi'
-$FSInstaller = 'FSLogixAppsSetup.zip'
-$Localpath = "c:\temp\wvd\"
+$WVDBootURI              = 'https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrxrH'
+$WVDAgentURI             = 'https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrmXv'
+$FSLogixURI              = 'https://go.microsoft.com/fwlink/?linkid=2084562'
+$Win7x86_UpdateURI       = 'https://download.microsoft.com/download/1/E/2/1E2A08C9-B87B-4808-94A6-30BC9D65775E/Windows6.1-KB2592687-x86.msu'
+$Win7x86_WMI5URI         = 'https://download.microsoft.com/download/6/F/5/6F5FF66C-6775-42B0-86C4-47D41F2DA187/Win7-KB3191566-x86.zip'
+$Win7x64_UpdateURI       = 'https://download.microsoft.com/download/A/F/5/AF5C565C-9771-4BFB-973B-4094C1F58646/Windows6.1-KB2592687-x64.msu'
+$Win7x64_WMI5URI         = 'https://download.microsoft.com/download/6/F/5/6F5FF66C-6775-42B0-86C4-47D41F2DA187/Win7AndW2K8R2-KB3191566-x64.zip'
+$Localpath               = "c:\temp\wvd\"
+$FSInstaller             = 'FSLogixAppsSetup.zip'
+$WVDAgentInstaller       = 'WVD-Agent.msi'
+$WVDBootInstaller        = 'WVD-Bootloader.msi'
+$Win7x86_UpdateInstaller = 'Win7-KB2592687-x86.msu'
+$Win7x86_WMI5Installer   = 'Win7-KB3191566-WMI5-x86.zip'
+$Win7x64_UpdateInstaller = 'Win7-KB2592687-x64.msu'
+$Win7x64_WMI5Installer   = 'Win7-KB3191566-WMI5-x64.zip'
 
 
 ####################################
@@ -66,8 +76,11 @@ Invoke-WebRequest -Uri $FSLogixURI -OutFile "$Localpath$FSInstaller"
 ##############################
 #    Prep for WVD Install    #
 ##############################
+Invoke-Expression `
+    -Command "cmdkey /add:msdean.file.core.windows.net /user:Azure\msdean /pass:M4DObmCRXxnn1Jm/ipwkJ4qhgJt7xoFH9cI7HGxKSp1u4+h8HnAAdTUvLXnKzjED5tcLDDTlHlzuGK+nAeesaQ==" `
+    -ErrorAction SilentlyContinue
 Expand-Archive `
-    -LiteralPath C:\temp\wvd\FSLogixAppsSetup.zip `
+    -LiteralPath "C:\temp\wvd\$FSInstaller" `
     -DestinationPath "$Localpath\FSLogix" `
     -Force `
     -Verbose
@@ -78,8 +91,9 @@ cd $Localpath
 ##############################
 #    OS Specific Settings    #
 ##############################
-If(((Get-CimInstance win32_operatingsystem).Name) -match 'server') {
-    "Windows Server OS Detected"
+$OS = (Get-CimInstance win32_operatingsystem).Name
+If(($OS) -match 'server') {
+    write-host -ForegroundColor Cyan -BackgroundColor Black "Windows Server OS Detected"
     If(((Get-WindowsFeature -Name RDS-RD-Server).installstate) -eq 'Installed') {
         "Session Host Role is already installed"
     }
@@ -99,8 +113,63 @@ If(((Get-CimInstance win32_operatingsystem).Name) -match 'server') {
     $SubKey.SetValue("IsInstalled",0,[Microsoft.Win32.RegistryValueKind]::DWORD)
 }
 Else {
-    "Windows Client OS Detected"
+    write-host -ForegroundColor Cyan -BackgroundColor Black "Windows Client OS Detected"
+    if(($OS) -match 'Windows 10') {
+        write-host `
+            -ForegroundColor Yellow `
+            -BackgroundColor Black  `
+            "Windows 10 detected...skipping to next step" 
+    }
+    else {
+        $OSArch = (Get-CimInstance win32_operatingsystem).OSArchitecture
+        If(($OSArch) -match '64-bit') {
+            write-host `
+                -ForegroundColor Magenta  `
+                -BackgroundColor Black `
+                "Windows 7 x64 detected"
+            $Win7x64_UpdateRequest = [System.Net.WebRequest]::Create($Win7x64_UpdateURI)
+            $Win7x64_WMI5Request   = [System.Net.WebRequest]::Create($Win7x64_WMI5URI)                
+            write-host `
+                -ForegroundColor Magenta `
+                -BackgroundColor Black `
+                "...installing Update KB2592687 for x64"
+            Expand-Archive `
+                -LiteralPath "C:\temp\wvd\$Win7x64_WMI5Installer" `
+                -DestinationPath "$Localpath\Win7Wmi5x64" `
+                -Force `
+                -Verbose
+            $packageName = 'Win7AndW2K8R2-KB3191566-x64.msu'
+            $packagePath = 'C:\temp\wvd\Win7Wmi5x64'
+            $wusaExe = "$env:windir\system32\wusa.exe"
+            $wusaParameters += @("/quiet", "/promptrestart")
+            $wusaParameterString = $wusaParameters -join " "
+            & $wusaExe $wusaParameterString
 
+        }
+        Else {
+            write-host `
+                -ForegroundColor Magenta  `
+                -BackgroundColor Black `
+                "Windows 7 x86 detected"
+            $Win7x86_UpdateRequest = [System.Net.WebRequest]::Create($Win7x32_UpdateURI)
+            $Win7x86_WMI5Request   = [System.Net.WebRequest]::Create($Win7x32_WMI5)              
+            write-host `
+                -ForegroundColor Magenta `
+                -BackgroundColor Black `
+                "...installing Update KB2592687 for x86"
+            Expand-Archive `
+                -LiteralPath "C:\temp\wvd\$Win7x86_WMI5Installer" `
+                -DestinationPath "$Localpath\Win7Wmi5x86" `
+                -Force `
+                -Verbose
+            $packageName = $Win7x86_WMI5Installer
+            $packagePath = 'C:\temp\wvd\Win7Wmi5x86'
+            $wusaExe = "$env:windir\system32\wusa.exe"
+            $wusaParameterString = $wusaParameters -join " "                
+            $wusaParameters += @("/quiet", "/promptrestart")
+            & $wusaExe $wusaParameterString
+        }
+    }
 }
 
 
@@ -156,12 +225,62 @@ New-Item `
     -Force 
 New-ItemProperty `
     -Path HKLM:\SOFTWARE\FSLogix\Profiles `
+    -Name "Enabled" `
+    -PropertyType "DWord" `
+    -Value 1
+New-ItemProperty `
+    -Path HKLM:\SOFTWARE\FSLogix\Profiles `
     -Name "DeleteLocalProfileWhenVHDShouldApply" `
     -PropertyType "DWord" `
     -Value 1
 New-ItemProperty `
     -Path HKLM:\SOFTWARE\FSLogix\Profiles `
-    -Name "Enabled" `
+    -Name "FlipFlopProfileDirectoryName" `
+    -PropertyType "DWord" `
+    -Value 0
+New-ItemProperty `
+    -Path HKLM:\SOFTWARE\FSLogix\Profiles `
+    -Name "PreventLoginWithFailure" `
+    -PropertyType "DWord" `
+    -Value 0
+New-ItemProperty `
+    -Path HKLM:\SOFTWARE\FSLogix\Profiles `
+    -Name "PreventLoginWithTempProfile" `
+    -PropertyType "DWord" `
+    -Value 0
+New-ItemProperty `
+    -Path HKLM:\SOFTWARE\FSLogix\Profiles `
+    -Name "IncludeOneDrive" `
+    -PropertyType "DWord" `
+    -Value 1
+New-ItemProperty `
+    -Path HKLM:\SOFTWARE\FSLogix\Profiles `
+    -Name "IncludeOneNote" `
+    -PropertyType "DWord" `
+    -Value 1
+New-ItemProperty `
+    -Path HKLM:\SOFTWARE\FSLogix\Profiles `
+    -Name "IncludeOneNote_UWP" `
+    -PropertyType "DWord" `
+    -Value 0
+New-ItemProperty `
+    -Path HKLM:\SOFTWARE\FSLogix\Profiles `
+    -Name "IncludeOutlook" `
+    -PropertyType "DWord" `
+    -Value 1
+New-ItemProperty `
+    -Path HKLM:\SOFTWARE\FSLogix\Profiles `
+    -Name "IncludeOutlookPersonalization" `
+    -PropertyType "DWord" `
+    -Value 1
+New-ItemProperty `
+    -Path HKLM:\SOFTWARE\FSLogix\Profiles `
+    -Name "IncludeSharepoint" `
+    -PropertyType "DWord" `
+    -Value 1
+New-ItemProperty `
+    -Path HKLM:\SOFTWARE\FSLogix\Profiles `
+    -Name "IncludeTeams" `
     -PropertyType "DWord" `
     -Value 1
 New-ItemProperty `
@@ -181,11 +300,11 @@ New-ItemProperty `
     -Value ""
 New-ItemProperty `
     -Path HKLM:\SOFTWARE\FSLogix\Profiles `
-    -Name "VHDLocations" `
+    -Name "CCDLocations" `
     -PropertyType "MultiString" `
-    -Value $ProfilePath
+    -Value "type=smb,connectionString=$ProfilePath"
 Pop-Location
- 
+
 
 #############
 #    END    #
